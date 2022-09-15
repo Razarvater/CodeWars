c8d3d1e3d7e714e5dc0a8e47a65ac5e8a55fb52b
Razarvater <rismangulov1.0@mail.ru>
2022-09-15 21:33:43 +0300

    Kata

---
+++ C#/6kyu/The Book of Mormon/The Book of Mormon.cs
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/58373ba351e3b615de0001c3
+
+public class Kata
+{
+  private static int MaxIter = 100;
+  public static long Mormons(long startingNumber, long reach, long target)
+  {
+    if(startingNumber>=target)return 0;
+    for(int i = 1;i<MaxIter;i++)
+      {
+          startingNumber += startingNumber*reach;
+        if(startingNumber>=target)return i;
+      }
+    return 0;
+  }
+}
